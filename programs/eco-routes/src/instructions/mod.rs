d41500a09a04a41d56cd56123d155a5027f7966b
KellianDev <kdev.solana@gmail.com>
2025-04-17 20:36:06 +0400

    add all ixs types

--- programs/eco-routes/src/instructions/mod.rs
+++ programs/eco-routes/src/instructions/mod.rs
@@ -1,12 +1,9 @@
-// intents
 pub mod close_intent;
-pub mod initialize_intent;
+pub mod fund_intent;
 pub mod publish_intent;
 
-// rewards
 pub mod claim_intent_reward;
-pub mod fund_intent_reward;
 pub mod refund_intent_reward;
 
-// inbox
+pub mod fulfill_and_prove;
 pub mod handle_inbound_fulfill;
