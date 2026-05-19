258bc618e7ee4496a2037fccc6e7f764c9f21d85
Elio Briceño <eliobricenov@gmail.com>
2026-05-19 13:25:51 -0400

    fix(portal): stream route hashing

--- programs/portal/src/types.rs
+++ programs/portal/src/types.rs
@@ -228,14 +228,32 @@ pub struct Route {
 
 impl Route {
     pub fn hash(&self) -> Bytes32 {
-        let encoded = self.try_to_vec().expect("Failed to serialize Route");
+        self.hash_streaming()
+    }
+
+    fn hash_streaming(&self) -> Bytes32 {
         let mut hasher = Keccak::v256();
         let mut hash = [0u8; 32];
 
-        hasher.update(&encoded);
+        self.update_hash(&mut hasher);
         hasher.finalize(&mut hash);
 
         hash.into()
+    }
+
+    fn update_hash(&self, hasher: &mut Keccak) {
+        hasher.update(self.salt.as_ref());
+        update_hash_u64(hasher, self.deadline);
+        hasher.update(self.portal.as_ref());
+        update_hash_u64(hasher, self.native_amount);
+        update_hash_len(hasher, self.tokens.len());
+        for token in &self.tokens {
+            token.update_hash(hasher);
+        }
+        update_hash_len(hasher, self.calls.len());
+        for call in &self.calls {
+            call.update_hash(hasher);
+        }
     }
 
     pub fn estimated_serialized_len(&self) -> usize {
@@ -303,10 +321,34 @@ pub struct TokenAmount {
     pub amount: u64,
 }
 
+impl TokenAmount {
+    fn update_hash(&self, hasher: &mut Keccak) {
+        hasher.update(self.token.as_ref());
+        update_hash_u64(hasher, self.amount);
+    }
+}
+
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
 pub struct Call {
     pub target: Bytes32,
     pub data: Vec<u8>,
+}
+
+impl Call {
+    fn update_hash(&self, hasher: &mut Keccak) {
+        hasher.update(self.target.as_ref());
+        update_hash_len(hasher, self.data.len());
+        hasher.update(&self.data);
+    }
+}
+
+fn update_hash_len(hasher: &mut Keccak, len: usize) {
+    let len = u32::try_from(len).expect("Borsh vec length exceeds u32");
+    hasher.update(&len.to_le_bytes());
+}
+
+fn update_hash_u64(hasher: &mut Keccak, value: u64) {
+    hasher.update(&value.to_le_bytes());
 }
 
 #[cfg(test)]
@@ -774,6 +816,42 @@ mod tests {
         };
 
         goldie::assert_json!(route.hash().as_ref());
+    }
+
+    #[test]
+    fn route_streaming_hash_matches_borsh_serialized_hash() {
+        let route = Route {
+            deadline: 1700000000,
+            salt: [1u8; 32].into(),
+            portal: [2u8; 32].into(),
+            native_amount: 10,
+            tokens: vec![
+                TokenAmount {
+                    token: Pubkey::new_from_array([3u8; 32]),
+                    amount: 100,
+                },
+                TokenAmount {
+                    token: Pubkey::new_from_array([4u8; 32]),
+                    amount: 200,
+                },
+            ],
+            calls: vec![
+                Call {
+                    target: [5u8; 32].into(),
+                    data: vec![1, 2, 3],
+                },
+                Call {
+                    target: [6u8; 32].into(),
+                    data: vec![4, 5, 6, 7, 8],
+                },
+            ],
+        };
+        let mut hasher = Keccak::v256();
+        let mut hash = [0u8; 32];
+        hasher.update(&route.try_to_vec().unwrap());
+        hasher.finalize(&mut hash);
+
+        assert_eq!(route.hash_streaming(), Bytes32::from(hash));
     }
 
     #[test]
